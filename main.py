5bd2048e726152436dc9bb740fd92ba0a0e99c23
Sssniperwolf <46751810+EnvGrn@users.noreply.github.com>
2020-04-01 00:55:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 users = {
 	"Envy" : [
 		"Envy",
-		hash("12345")
+		hash("cfad44646")
 	]
 }
 
@@ -30,19 +30,22 @@ def tekdm():
 def register():
     if request.method == "POST":
         form = request.form
-
-        if len(form.psw) > 16 or len(form.psw) < 4:
+        print("owowowowowoow")
+        if len(form["password"]) > 16 or len(form["password"]) < 4:
+            print("owowowowoow")
             return flask.render_template("register.html", error="Lösenordet måste vara mellan 4 och 16 tecken")
-        elif form.psw != form.psw2:
+        elif form["password"] != form["password2"]:
+            print("owowowowoow")
             return flask.render_template("register.html", error="Lösenorden ska matcha!")
-        elif form.email in users:
+        elif form["email"] in users:
+            print("owowowowoow")
             return flask.render_template("register.html", error="Det finns redan ett konto med nedskriven email")
         else:
-            users[form.email] = [form.email, hash(form.psw)]
+            users[form["email"]]= form["email"], hash(form["password"])
             return flask.render_template("login.html")
     else:
         return flask.render_template("register.html")
-		
+
 # hanterar vad som händer när route är "/login"
 # accepterar requests med metoderna get och post
 # get för att hämta login sida, post för att ladda upp saker till den
@@ -52,18 +55,25 @@ def login():
 	# om metoden är post
 	if request.method == "POST":
 		form = request.form
+		print("Hej")
+		print(form)
+		print(form["email"])
 
 		# ha en form på hemsidan som har två fält för inlogning
-		# password för lösenord och username för användarnamn
+		# ["password"] för lösenord och username för användarnamn
 
 		# kolla om lösenordet finns
-		if form.email in users:
+		if form["email"] in users:
+			print("jag äter katter")
 			#kolla om lösenorden stämmer
-			if hash(form.psw) == users[form.email][1]:
+			if hash(form["password"]) == users[form["email"]][1]:
+				print("mitt liv är över")
 				# filväg för den in loggade hesidan som man ska se om man är inloggad
 				return flask.redirect("index.html")
+		print("jag äter kattpojkar")
 		return flask.render_template("login.html",  error="Någonting är fel, försök igen")
 	# visa FILVÄG för användaren (eftersom get metoder kommer igenom)
+	print("jag har ätit alla kattpojkar")
 	return flask.render_template("login.html") #filväg för login.html
 
-app.run()
+app.run(debug = True)
